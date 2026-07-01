9a185e8cbf484ea100d36120093a253facd99d8a
StanEM75 <standhuart75@gmail.com>
2026-07-01 21:59:31 +0200

    feat: initialize the dbt project for transforming data

---
+++ transformation_of_all_data/models/staging/stg_stock_data.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized='view',
+        schema='STG_FINANCIAL_DATA',
+        tags=['staging', 'stock_data'],
+    )
+}}
+
+SELECT 
+        date,
+        symbol,
+        name AS company_name,
+        asset_type,
+        price_currency,
+        open AS open_price,
+        high AS high_price,
+        low AS low_price,
+        close AS close_price,
+        volume AS nb_shares_traded_during_the_day,
+        adj_open AS adjusted_open_price,
+        adj_high AS adjusted_high_price,
+        adj_low AS adjusted_low_price,
+        adj_close AS adjusted_close_price,
+        adj_volume AS adjusted_nb_shares_traded_during_the_day,
+        dividend AS dividend_amount,
+        split_factor,
+        exchange AS stock_exchange_name,
+        exchange_code AS stock_exchange_code
+FROM 
+        {{ source('raw', 'raw_stock_data') }}
+        
