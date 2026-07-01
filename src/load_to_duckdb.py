3c612a5ec5087cf659ac8714781968729f58a772
StanEM75 <standhuart75@gmail.com>
2026-07-01 21:58:33 +0200

    fix: fix data paths

--- src/load_to_duckdb.py
+++ src/load_to_duckdb.py
@@ -1,30 +1,44 @@
+# ================================================================================
+#                                     PACKAGES
+# ================================================================================
+
+from pathlib import Path
 import duckdb
 
-def load_raw_tables():
-    con = duckdb.connect("../warehouse/financial.duckdb")
+# ================================================================================
+#                                INSTANTIATE THE PATH
+# ================================================================================
 
-    con.execute("""
+ROOT = Path(__file__).resolve().parents[1]
+
+DB_PATH = ROOT / "financial.duckdb"
+DATA_PATH = ROOT / "data"
+
+def load_raw_tables():
+    con = duckdb.connect(str(DB_PATH))
+
+    con.execute(f"""
         CREATE OR REPLACE TABLE raw_stock_data AS
         SELECT *
-        FROM read_csv_auto('data/stock_data.csv', HEADER=TRUE)
+        FROM read_csv_auto('{DATA_PATH / "stock_data.csv"}', HEADER=TRUE)
     """)
 
-    con.execute("""
+    con.execute(f"""
         CREATE OR REPLACE TABLE raw_statement_data AS
         SELECT *
-        FROM read_csv_auto('data/df_statement.csv', HEADER=TRUE)
+        FROM read_csv_auto('{DATA_PATH / "df_statement.csv"}', HEADER=TRUE)
     """)
 
-    con.execute("""
+    con.execute(f"""
         CREATE OR REPLACE TABLE raw_open_positions_data AS
         SELECT *
-        FROM read_csv_auto('data/df_open_positions.csv', HEADER=TRUE)
+        FROM read_csv_auto('{DATA_PATH / "df_open_positions.csv"}', HEADER=TRUE)
     """)
 
-    con.execute("""
+    con.execute(f"""
         CREATE OR REPLACE TABLE raw_performance_data AS
         SELECT *
-        FROM read_csv_auto('data/df_performance.csv', HEADER=TRUE)
+        FROM read_csv_auto('{DATA_PATH / "df_performance.csv"}', HEADER=TRUE)
     """)
 
     con.close()
