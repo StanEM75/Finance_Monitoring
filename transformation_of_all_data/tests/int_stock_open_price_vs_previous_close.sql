baed24710054b803fc230e4266c5592ce5ddcae4
StanEM75 <standhuart75@gmail.com>
2026-07-04 19:13:26 +0200

    feat: add test to check relevancy of open vs close prices

---
+++ transformation_of_all_data/tests/int_stock_open_price_vs_previous_close.sql
@@ -0,0 +1,47 @@
+{{
+    config(
+        severity='warn',
+        store_failures=true,
+        schema='DBT_TESTS',
+        alias='int_stock_open_price_vs_previous_close'
+    )
+}}
+
+-- ================================================================================================================
+-- Check that the opening price does not differ by more than 10% from the previous trading day's closing price.
+-- This test helps detect potential data quality issues such as missing stock split adjustments,
+-- incorrect prices, or erroneous records returned by the data provider.
+-- ================================================================================================================
+
+WITH stock_with_previous_close AS 
+(
+    SELECT
+            date,
+            symbol,
+            open_price,
+            close_price,
+            LAG(close_price) OVER (
+                PARTITION BY symbol
+                ORDER BY date
+            ) AS previous_close_price
+
+    FROM 
+            {{ ref('int_stock') }}
+
+),
+
+invalid_rows AS 
+(
+    SELECT 
+            *
+    FROM 
+            stock_with_previous_close
+    WHERE 
+            previous_close_price IS NOT NULL
+            AND ABS(open_price - previous_close_price) > 0.10 * previous_close_price
+)
+
+SELECT 
+        *
+FROM 
+        invalid_rows
